440bdea3cc65187b3c84339a48049a2cf1840a1e
HisaJi20 <avilaguerrapatriciajimena20@gmail.com>
2026-09-16 11:39:47 -0500

    Inicialización del proyecto AccesIA con Spring Boot

---
+++ src/main/java/com/accesia/accesia/AccesiaApplication.java
@@ -0,0 +1,13 @@
+package com.accesia.accesia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accesia/accesia/AccesiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accesia.accesia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
